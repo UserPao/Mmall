e4d44810128221df4397bf053e87d7c8757fcfbc
konghui <245061377@qq.com>
2019-05-06 17:39:46 +0800

    第7次提交--增加session重置时间

---
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,48 @@
+package com.mmall.controller.common;
+
+import com.mmall.common.Const;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @Author: huki-konghui
+ * @Date: 2019/5/6 17:31
+ * @Version 1.0
+ */
+public class SessionExpireFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)servletRequest;
+
+        String loginToken = CookieUtil.readLoginCookie(httpServletRequest);
+        if (StringUtils.isNotEmpty(loginToken)) {
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user  = JsonUtil.string2Obj(userJsonStr,User.class);
+            if(user != null){
+                //if user is not null,to reset the time of session,to call the expire command
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_TIME);
+            }
+        }
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
