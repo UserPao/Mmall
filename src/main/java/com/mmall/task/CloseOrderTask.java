e1e8d05f23be38d26fdba48b5d654ad77924b87f
konghui <245061377@qq.com>
2019-05-08 15:57:15 +0800

    第9次提交--分布式锁

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -22,7 +22,7 @@ public class CloseOrderTask {
     @Autowired
     private IOrderService iOrderService;
 
-    @Scheduled(cron ="0 */1 * * * ?")
+//    @Scheduled(cron ="0 */1 * * * ?")
     //每一分钟执行一次,关闭两个小时之前开启的订单，但是为付款的订单
     public void closeOrderTaskV1(){
         log.info("关闭订单定时任务启动");
@@ -31,7 +31,7 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
-
+    @Scheduled(cron ="0 */1 * * * ?")
     public void closeOrderTaskV2(){
         log.info("关闭订单定时任务启动");
         long lockTimeOut =Long.parseLong(PropertiesUtil.getProperty("lock.timeout","5000"));
@@ -49,7 +49,7 @@ public class CloseOrderTask {
         RedisShardedPoolUtil.expire(lockName,50);//有效期50秒，防止死锁
         log.info("获取{}，ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
         int hour =Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
-        iOrderService.closeOrder(hour);
+//        iOrderService.closeOrder(hour);
         RedisShardedPoolUtil.del(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
         log.info("释放{}，ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
         log.info("=============================");
