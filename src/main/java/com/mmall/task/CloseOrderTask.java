d6553c76fdd5250eb9adb865f531b51bcb009961
konghui <245061377@qq.com>
2019-05-08 15:56:19 +0800

    第9次提交--分布式锁

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -1,7 +1,10 @@
 package com.mmall.task;
 
+import com.mmall.common.Const;
+import com.mmall.common.RedisShardedPool;
 import com.mmall.service.IOrderService;
 import com.mmall.util.PropertiesUtil;
+import com.mmall.util.RedisShardedPoolUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -28,4 +31,27 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
+
+    public void closeOrderTaskV2(){
+        log.info("关闭订单定时任务启动");
+        long lockTimeOut =Long.parseLong(PropertiesUtil.getProperty("lock.timeout","5000"));
+        Long setnxResult = RedisShardedPoolUtil.setnx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,String.valueOf(System.currentTimeMillis() + lockTimeOut));
+        if(setnxResult != null && setnxResult.intValue() ==1){
+            //如果返回值为1，代表设置成功，获取锁
+            this.closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        }else{
+            log.info("没有获取分布式锁：{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        }
+        log.info("关闭订单定时任务结束");
+    }
+
+    private void closeOrder(String lockName){
+        RedisShardedPoolUtil.expire(lockName,50);//有效期50秒，防止死锁
+        log.info("获取{}，ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
+        int hour =Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
+        iOrderService.closeOrder(hour);
+        RedisShardedPoolUtil.del(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        log.info("释放{}，ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
+        log.info("=============================");
+    }
 }
